c5edd509afac61402e4c546b8ba7b85400d4f104
konradlubelski <62030308+konradlubelski@users.noreply.github.com>
2020-03-19 16:22:48 +0100

    Random filtred even numbers

--- empty list.py
+++ empty list.py
@@ -14,15 +14,11 @@ def generate_random_array_with_numbers(len_of_elems):
 
 def generate_random_array_with_numbers_1(len_of_elems):
     list_of_10_random_number_1=[]
-    for condition_1 in range (1,11):
+    for _condition_1 in range (1,11):
         random_number_1=random.randint(1,100)
         list_of_10_random_number_1.append(random_number_1)
     print (list_of_10_random_number_1)
     print(len(list_of_10_random_number_1))
 
-generate_random_array_with_numbers(10)
-generate_random_array_with_numbers_1(10)
-generate_random_array_with_numbers(10)
-generate_random_array_with_numbers_1(10)
 generate_random_array_with_numbers(10)
 generate_random_array_with_numbers_1(10)
